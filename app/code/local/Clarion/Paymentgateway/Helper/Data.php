8867b5d71d162763c807ee12679697ee1ba6643c
crazyquark <sam.bitcoin@gmail.com>
2015-09-09 16:30:12 +0300

    add account email

--- app/code/local/Clarion/Paymentgateway/Helper/Data.php
+++ app/code/local/Clarion/Paymentgateway/Helper/Data.php
@@ -5,7 +5,12 @@ class Clarion_Paymentgateway_Helper_Data extends Mage_Core_Helper_Abstract
 
 public function getPaymentmethodPaymentgatewayurl()
 {
-return Mage::getStoreConfig("payment/paymentgateway/cgi_url");
+	return Mage::getStoreConfig("payment/paymentgateway/cgi_url");
+}
+
+public function getAccountEmail()
+{
+	return Mage::getStoreConfig("payment/paymentgateway/account_email");
 }
    
     
